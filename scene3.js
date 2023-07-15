5bb4034edf25e84fe98ffff73da0dbfe19796411
Qinxi <qinxiw2@illinois.edu>
2023-07-14 23:32:04 -0700

    slider bar s3

--- scene3.js
+++ scene3.js
@@ -1,1 +1,9 @@
 // filter by type and year change
+const slider = d3.select("#slider");
+const output = d3.select("body").append("p");
+// default init
+output.text(1970);
+// Update the output value when the slider changes
+slider.on("input", function() {
+    output.text(this.value);
+});
